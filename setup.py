a12555b0b1cea79a644caf04aa1497465a4e65b0
Julian Zgoda <julian.zgoda@rubrik.com>
2023-11-06 16:25:36 -0700

    WIP

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name='rscOracleTools',
-    packages=find_packages(include=['rubrik_rsc_oracle']),
+    packages=find_packages(include=['rsc_oracle']),
     version='0.1.0',
     description='Package of tools to use Rubrik GraphQL API for Oracle',
     author='Julian Zgoda',
@@ -9,7 +9,7 @@ setup(
     install_requires=[
         'requests >= 2.18.4, != 2.22.0',
         'urllib3 >= 1.26.5',
-        'gql'
+        'gql',
         'Click',
         'pytz',
         'yaspin',
